f7f732e9456c672833fa0812b83daf21fd6030a2
YCM <min940913@naver.com>
2018-02-28 21:11:59 +0900

    2017-02-28
    블랜딩

---
+++ 프레임워크 1.7ver 블랜딩/Scene.cpp
@@ -0,0 +1,252 @@
+#include "Scene.h"
+
+
+Scene::Scene(ID3D12Device * m_Device, ID3D12GraphicsCommandList * m_DC, float cw, float ch)
+{
+
+	device = m_Device;
+	commandlist = m_DC;
+
+	mWidth = cw;
+	mHeight = ch;
+	
+	XMFLOAT3 e(0, 60, -90);
+	XMFLOAT3 a(0, 0, 0);
+	XMFLOAT3 u(0, 1, 0);
+	
+	//루트시그니처와 쉐이더들을 생성한다.
+
+	CreateRootSignature();
+	nShader = 1;
+	Shaders = new Shader*[nShader];
+	CreateShaderObject();
+	Player = new CPlayer(m_Device, m_DC, cw / ch, e, a, u);
+	Light = new CLight(m_Device, m_DC); //
+	CreateGameObject();
+
+
+
+}
+
+
+Scene::Scene()
+{
+}
+
+
+Scene::~Scene()
+{
+	if (Shaders != NULL)
+	{
+		for (int i = 0; i < nShader; i++)
+		{
+			delete Shaders[i];
+		}
+		delete[] Shaders;
+	}
+	while (DynamicObject.size())
+	{
+		delete DynamicObject.front();
+		DynamicObject.pop_front();
+	}
+}
+
+
+void Scene::CreateRootSignature()
+{
+	CD3DX12_DESCRIPTOR_RANGE texTable;
+	texTable.Init(D3D12_DESCRIPTOR_RANGE_TYPE_SRV, 1, 0);
+
+	
+	//연결될 리소스들 목록. 현재는 큐브의 월드변환행렬 하나만 넣을것.
+	CD3DX12_ROOT_PARAMETER RootParameter[5];
+
+	//파라메터의 종류는 3가진데  디스크립터쓸거임
+
+
+	//파라메터목록에 해당 목록을 인자로 넣으면서 초기화함
+	RootParameter[0].InitAsDescriptorTable(1, &texTable, D3D12_SHADER_VISIBILITY_PIXEL);
+
+	RootParameter[1].InitAsConstantBufferView(0); //obj     //register b0
+	RootParameter[2].InitAsConstantBufferView(1); //joint   //b1
+	RootParameter[3].InitAsConstantBufferView(2); //camera  //b2
+	RootParameter[4].InitAsConstantBufferView(3); //light
+
+
+	auto staticSamplers = GetStaticSamplers();
+
+	// 루트시그니처는 루트파라메터의 배열이다.
+	//루트시그니처의 정보구조체 생성
+	CD3DX12_ROOT_SIGNATURE_DESC rootSigDesc(5, RootParameter, (UINT)staticSamplers.size(), staticSamplers.data(),
+		D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT);
+	
+
+	// 루트시그니처를 만들 메모리 구조
+	ComPtr<ID3DBlob> serializedRootSig = nullptr;
+	ComPtr<ID3DBlob> errorBlob = nullptr;
+	
+	//리소스들을 직렬화한다. 예를들어 버퍼가 2개들어가면 b0,b1 이렇게 순서대로 들어감.
+	HRESULT hr = D3D12SerializeRootSignature(&rootSigDesc, D3D_ROOT_SIGNATURE_VERSION_1,
+		serializedRootSig.GetAddressOf(), errorBlob.GetAddressOf());
+
+	if (errorBlob != nullptr)
+	{
+		::OutputDebugStringA((char*)errorBlob->GetBufferPointer());
+	}
+	ThrowIfFailed(hr);
+
+	ThrowIfFailed(device->CreateRootSignature(
+		0,
+		serializedRootSig->GetBufferPointer(),
+		serializedRootSig->GetBufferSize(),
+		IID_PPV_ARGS(&rootsg)));
+	
+}
+
+void Scene::CreateShaderObject()
+{
+	for (int i = 0; i < nShader; i++)
+	{
+		Shaders[i] = new Shader;
+		Shaders[i]->CreateShader(device, rootsg.Get(), L"ShaderFile.hlsl", "VS", "vs_5_0", L"ShaderFile.hlsl", "PS", "ps_5_0");
+		Shaders[i]->DynamicObject = &DynamicObject;
+	
+	}
+	
+}
+
+void Scene::CreateGameObject()
+{
+	DynamicObject.push_back(new CZombieObject(device, commandlist));
+	DynamicObject.push_back(new CZombieObject(device, commandlist, XMFLOAT4(7, 8, 0, 0)));
+	DynamicObject.push_back(new CZombieObject(device, commandlist, XMFLOAT4(-7,0, 10, 0)));
+	//플레이어의 오브젝트 설정. 이건 나중에 바꿔야함.
+	Player->SetPlayer(DynamicObject.front());
+	//블랜딩용 테스트코드
+	Player->PlayerObject->Blending = true;
+
+	for (int i = 0; i < nShader; i++)
+	{
+		Shaders[i]->DynamicObject = &DynamicObject;
+	}
+
+}
+
+void Scene::Render()
+{
+
+	//나중에 카메라에 뷰포트와 씨저렉트를 갖도록하고 여기에서 연결하도록하자
+
+	//루트시그니처연결. 이때 루트시그니처는 쉐이더 종류에 관계없이 동일해야함.
+	commandlist->SetGraphicsRootSignature(rootsg.Get());
+
+
+	//이제 쉐이더를 연결하고 PSO 연결후 쉐이더의 렌더함수호출. 그다음 게임오브젝트의 상수버퍼(시그니처에서 했던거)를 연결해야함
+	//테이블의 경우 힙과 테이블을 연결해야함.
+	if (nShader > 0)
+	{
+		for (int i = 0; i < nShader; i++)
+		{
+			Shaders[i]->SetShader(commandlist);
+			//여기까지 오면  정점버퍼, 인덱스버퍼 , 상수버퍼뷰만 연결하면 된다.
+
+			//여기에 카메라행렬,프로젝션행렬을 추가한다.
+			Player->Camera.UpdateConstantBuffer(commandlist);
+			
+			Light->UpdateConstantBuffer(commandlist);
+			
+			//쉐이더가 보유한 그려야할 오브젝트 목록을 그린다.
+			Shaders[i]->Render(commandlist);
+			
+		}
+	}
+}
+
+void Scene::Tick(const GameTimer & gt)
+{
+	//플레이어 입력처리 
+	Player->PlayerInput(gt.DeltaTime());
+
+
+	//지워야 할 오브젝트를 지운다.
+	for (auto i = DynamicObject.begin(); i != DynamicObject.end();)
+	{
+
+		if ((*i)->DelObj==true)
+		{
+			delete *i;//실제 게임오브젝트의 메모리 해제
+			i = DynamicObject.erase(i);//리스트상에서 해당 요소를 지움
+
+
+		}
+		else
+			i++;
+	}
+
+
+	//오브젝트들의 틱함수 처리
+	for (auto b = DynamicObject.begin(); b != DynamicObject.end(); b++)
+		(*b)->Tick(gt);
+
+	//카메라 리 로케이트 
+	Player->PlayerCameraReLocate();
+}
+
+
+array<const CD3DX12_STATIC_SAMPLER_DESC, 6> Scene::GetStaticSamplers()
+{
+
+	// Applications usually only need a handful of samplers.  So just define them all up front
+	// and keep them available as part of the root signature.  
+
+	const CD3DX12_STATIC_SAMPLER_DESC pointWrap(
+		0, // shaderRegister
+		D3D12_FILTER_MIN_MAG_MIP_POINT, // filter
+		D3D12_TEXTURE_ADDRESS_MODE_WRAP,  // addressU
+		D3D12_TEXTURE_ADDRESS_MODE_WRAP,  // addressV
+		D3D12_TEXTURE_ADDRESS_MODE_WRAP); // addressW
+
+	const CD3DX12_STATIC_SAMPLER_DESC pointClamp(
+		1, // shaderRegister
+		D3D12_FILTER_MIN_MAG_MIP_POINT, // filter
+		D3D12_TEXTURE_ADDRESS_MODE_CLAMP,  // addressU
+		D3D12_TEXTURE_ADDRESS_MODE_CLAMP,  // addressV
+		D3D12_TEXTURE_ADDRESS_MODE_CLAMP); // addressW
+
+	const CD3DX12_STATIC_SAMPLER_DESC linearWrap(
+		2, // shaderRegister
+		D3D12_FILTER_MIN_MAG_MIP_LINEAR, // filter
+		D3D12_TEXTURE_ADDRESS_MODE_WRAP,  // addressU
+		D3D12_TEXTURE_ADDRESS_MODE_WRAP,  // addressV
+		D3D12_TEXTURE_ADDRESS_MODE_WRAP); // addressW
+
+	const CD3DX12_STATIC_SAMPLER_DESC linearClamp(
+		3, // shaderRegister
+		D3D12_FILTER_MIN_MAG_MIP_LINEAR, // filter
+		D3D12_TEXTURE_ADDRESS_MODE_CLAMP,  // addressU
+		D3D12_TEXTURE_ADDRESS_MODE_CLAMP,  // addressV
+		D3D12_TEXTURE_ADDRESS_MODE_CLAMP); // addressW
+
+	const CD3DX12_STATIC_SAMPLER_DESC anisotropicWrap(
+		4, // shaderRegister
+		D3D12_FILTER_ANISOTROPIC, // filter
+		D3D12_TEXTURE_ADDRESS_MODE_WRAP,  // addressU
+		D3D12_TEXTURE_ADDRESS_MODE_WRAP,  // addressV
+		D3D12_TEXTURE_ADDRESS_MODE_WRAP,  // addressW
+		0.0f,                             // mipLODBias
+		8);                               // maxAnisotropy
+
+	const CD3DX12_STATIC_SAMPLER_DESC anisotropicClamp(
+		5, // shaderRegister
+		D3D12_FILTER_ANISOTROPIC, // filter
+		D3D12_TEXTURE_ADDRESS_MODE_CLAMP,  // addressU
+		D3D12_TEXTURE_ADDRESS_MODE_CLAMP,  // addressV
+		D3D12_TEXTURE_ADDRESS_MODE_CLAMP,  // addressW
+		0.0f,                              // mipLODBias
+		8);                                // maxAnisotropy
+
+	return {
+		pointWrap, pointClamp,
+		linearWrap, linearClamp,
+		anisotropicWrap, anisotropicClamp };
+}
