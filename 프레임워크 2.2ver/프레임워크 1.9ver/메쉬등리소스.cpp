50febec1db4811dc47859d907a1875e552c5be22
YCM <min940913@naver.com>
2018-03-07 00:45:12 +0900

    피킹 수정

---
+++ 프레임워크 2.2ver/프레임워크 1.9ver/메쉬등리소스.cpp
@@ -0,0 +1,18 @@
+#include"CGameObject.h"
+
+
+bool CCubeManObject::CreateMesh=false;
+unordered_map<string, unique_ptr<CTexture>> CCubeManObject::Textures;//텍스처들을 저장함
+CMesh  CCubeManObject::Mesh;//나중에 메쉬는 static으로 둬야함
+std::vector<ModelAnimation> CCubeManObject::animations;//애니메이션 데이터 저장. 메쉬와 이거,텍스처는 한번만생성해서 공유하도록해야됨
+ComPtr<ID3D12DescriptorHeap> CCubeManObject::SrvDescriptorHeap;//텍스처 용 힙
+CMaterial CCubeManObject::Mat;
+
+//--------------------------------------------------------------------------------------
+
+bool CZombieObject::CreateMesh = false;
+unordered_map<string, unique_ptr<CTexture>> CZombieObject::Textures;//텍스처들을 저장함
+CMesh  CZombieObject::Mesh;//나중에 메쉬는 static으로 둬야함
+std::vector<ModelAnimation> CZombieObject::animations;//애니메이션 데이터 저장. 메쉬와 이거,텍스처는 한번만생성해서 공유하도록해야됨
+ComPtr<ID3D12DescriptorHeap> CZombieObject::SrvDescriptorHeap;//텍스처 용 힙
+CMaterial CZombieObject::Mat;
