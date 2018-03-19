8fd29360c161b09c515aed6990aa393a87856108
YCM <min940913@naver.com>
2018-03-19 22:14:45 +0900

    2018-03-19
    GeometryShader

---
+++ 유창민/프레임워크 2.4/CVertex.cpp
@@ -0,0 +1,27 @@
+#include "CVertex.h"
+
+
+CVertex::CVertex(void)
+{
+	XMVECTOR t = XMVectorZero();
+	XMStoreFloat3(&V, t);
+	XMStoreFloat4(&N, t);
+}
+
+
+CVertex::~CVertex(void)
+{
+}
+
+
+
+BillVertex::BillVertex(void)
+{
+	XMVECTOR t = XMVectorZero();
+	XMStoreFloat3(&V, t);
+	XMStoreFloat2(&Size, t);
+}
+
+BillVertex::~BillVertex(void)
+{
+}
