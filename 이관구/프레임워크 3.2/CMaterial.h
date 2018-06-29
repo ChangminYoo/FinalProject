98ecee814561d6be0eab76f0f31c0aa0271f2b3b
dufdif <klkb1@naver.com>
2018-06-29 15:03:05 +0900

    2018-06-29
    바닥에 노멀매핑 적용완료

---
+++ 이관구/프레임워크 3.2/CMaterial.h
@@ -0,0 +1,26 @@
+#pragma once
+#include"UploadBuffer.h"
+
+using namespace DirectX;
+
+struct MaterialData
+{
+	XMFLOAT4 Diffuse = { 1.0f, 1.0f, 1.0f, 1.0f };
+	float Roughness = 0.25f;	//°ÅÄ¥±â
+	XMFLOAT3 CustomMaterial = { 0,0,0 };
+	XMFLOAT4 Emissive = { 0, 0, 0, 1.0f };
+};
+
+
+class CMaterial
+{
+public:
+	CMaterial(ID3D12Device* Device, ID3D12GraphicsCommandList* commandlist);
+	CMaterial();
+	~CMaterial();
+
+	void UpdateConstantBuffer(ID3D12GraphicsCommandList* commandlist);
+	UploadBuffer<MaterialData>* ConstBuffer = nullptr;
+	MaterialData MatData;
+};
+
